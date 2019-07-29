0c06189072e1ad4c493c007c678729b03c2ef4d0
pegasy <pegasy@users.noreply.github.com>
2019-07-29 23:17:47 +0200

    add push script

---
+++ push.sh
@@ -0,0 +1,4 @@
+docker push pegasy/lancachenet_lancache-dns:latest
+docker push pegasy/lancachenet_monolithic:latest
+docker push pegasy/lancachenet_sniproxy:latest
+docker push pegasy/lancachenet_generic:latest
